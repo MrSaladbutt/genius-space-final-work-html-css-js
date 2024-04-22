8984d8b217d60494772322cc13e9c374100b0c61
MrSaladbutt <saladbutt@email.ua>
2024-04-22 21:29:31 +0300

    4

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="https://mrsaladbutt.github.io/">
       <App />
     </Router>
   </React.StrictMode>,
